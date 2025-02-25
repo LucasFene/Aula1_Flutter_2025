083dcab6b9c6af30e84868371bc99544b0d587a8
Lucas Fernandes <lucas.fernandes.df462@gmail.com>
2025-02-25 00:08:50 -0300

    .

--- lib/contato.dart
+++ lib/contato.dart
@@ -3,5 +3,21 @@ class Contact {
   String name;
   String phone;
 
-  
+  Contact({this.id, required this.name, required this.phone});
+
+  Map<String, dynamic> toMap(){
+    return {
+      'id': id,
+      'name': name,
+      'phone': phone,
+    };
+  }
+
+  factory Contact.fromMap(Map<String, dynamic> map){
+    return Contact(
+      id: map['id'],
+      name: map['name'],
+      phone: map['phone'],
+    );
+  }
 }
